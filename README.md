f17c7fe986dbfa0f0c82cd540f3656f12417dbc5
Marco Pivetta <ocramius@gmail.com>
2012-07-16 01:26:02 +0200

    Moving to a jekyll-only version based on @erjjones work

---
+++ README.md
@@ -0,0 +1,5 @@
+## ocramius.github.com
+
+This is [my personal GitHub page](http://ocramius.github.com/) made with Jekyll and based on
+[Eric Jones' work](http://erjjones.github.com/).
+I will post my thoughts about what I do here, but I also accepts any fixes to my articles/tutorials/guides.
---
+++ js/custom.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function() {
+
+	$('#gf').text('GitHub Followers');
+    $('#gfr').text('GitHub Repos');
+
+	JSONP( 'https://api.github.com/users/ocramius?callback=?', function( response ) {
+		var data = response.data;
+		$('#gf').text(data.followers + ' GitHub Followers');
+        $('#gfr').text(data.public_repos + ' GitHub Repos');
+	});
+
+	function JSONP( url, callback ) {
+		var id = ( 'jsonp' + Math.random() * new Date() ).replace('.', '');
+		var script = document.createElement('script');
+		script.src = url.replace( 'callback=?', 'callback=' + id );
+		document.body.appendChild( script );
+		window[ id ] = function( data ) {
+			if (callback) {
+				callback( data );
+			}
+		};
+	}
+
+	$('#ghw').githubWidget({
+			'username': 'Ocramius',
+			'displayActions': false,
+			'firstCount': 10,
+			'displayHeader': false,
+			'displayLastCommit': false,
+			'displayAccountInformations': false,
+			'displayLanguage': false
+		});
+});
