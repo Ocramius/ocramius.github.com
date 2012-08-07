5e6e6a3a4016ae715c37dff61ca884d6b94034fe
Marco Pivetta <ocramius@gmail.com>
2012-08-07 20:40:54 +0200

    Using prettify-prepro for PHP highlighting

---
+++ js/google-code-prettify/prettify-prepro.js
@@ -0,0 +1,16 @@
+PR.registerLangHandler(PR.sourceDecorator({
+    keywords:       (
+        'abstract and array as break case catch class clone const continue declare '
+            +
+            'default do else elseif enddeclare endfor endforeach endif endswitch endwhile extends final '
+            +
+            'for foreach function global goto if implements interface instanceof namespace new or private '
+            +
+            'protected public static switch throw try use var while xor '
+            +
+            '__CLASS__ __DIR__ __FILE__ __LINE__ __FUNCTION__ __METHOD__ __NAMESPACE__ '
+            +
+            'die echo empty exit eval include include_once isset list require require_once return print unset'
+        ),
+    cStyleComments: true
+}), ['prepro']);
