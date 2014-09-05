cf61b68423a0290e902132916e3c92249992dfd1
malukenho <admin@phpse.net>
2014-09-05 06:48:19 -0300

    Starting to create a github bundle

---
+++ app/SculpinKernel.php
@@ -0,0 +1,11 @@
+<?php
+
+class SculpinKernel extends \Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel
+{
+    protected function getAdditionalSculpinBundles()
+    {
+        return array(
+            'Bundles\Github\Repositoroies'
+        );
+    }
+}
