3f3cc66f189c0935d823ee185ef26bd22109fb11
malukenho <admin@phpse.net>
2014-09-10 00:02:14 -0300

    Create css and html to display repositories

--- src/GithubBundle/Template/Repositories.php
+++ src/GithubBundle/Template/Repositories.php
@@ -1,7 +1,13 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: admin
- * Date: 09/09/14
- * Time: 23:28
- */ 
+ * Template for show repositories on github blog page
+ */
+$date = new DateTime($repository->updated_at);
+return <<<EOF
+<div class="span11 github-repository">
+    <h3>{$repository->name}</h3>
+    <p><small><a href="{$repository->html_url}" target="_blank">
+        {$repository->full_name}</a> - last update: {$date->format('Y-m-d')}</small></p>
+    <p class="description">{$repository->description}</p>
+</div>
+EOF;
