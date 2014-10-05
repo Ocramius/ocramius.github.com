04e875e444904ab0ae650ce95d499b071694b03f
Marco Pivetta <ocramius@gmail.com>
2014-10-05 04:52:19 +0200

    Deploying sculpin-generated pages to `master` branch

--- makefile
+++
@@ -1,8 +0,0 @@
-sculpin: sculpin.json sculpin.lock
-	curl -sS https://getcomposer.org/installer | php
-
-install: source/
-	php vendor/bin/sculpin install
-
-setup: .sculpin
-	php vendor/bin/sculpin generate --watch --server
---
+++ presentations/doctrine-orm-and-zend-framework-2/Gruntfile.js
@@ -0,0 +1,66 @@
+// Generated on 2013-10-01 using generator-reveal 0.0.15
+'use strict';
+
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: LIVERELOAD_PORT
+            },
+            livereload: {
+                files: [
+                    'index.html',
+                    'js/*.js',
+                    'slides/*.md',
+                    'slides/*.html',
+                    'slides/list.json'
+                ],
+                tasks: ['build']
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        }
+    });
+
+    grunt.registerTask('server', ['build', 'connect:livereload', 'open', 'watch']);
+
+    grunt.registerTask('build', 'Build your slides.', function () {
+        var slides = [];
+
+        grunt.file.recurse('slides', function (post, root, sub, fileName) {
+            if (fileName === 'index.md') {
+                return;
+            }
+        });
+    });
+};
