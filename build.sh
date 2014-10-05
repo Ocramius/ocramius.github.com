06e8f847de18b3c15bf13066a59c5b48d9b6d7cb
Marco Pivetta <ocramius@gmail.com>
2014-10-05 04:22:20 +0200

    Clearing directories before building

--- build.sh
+++ build.sh
@@ -5,8 +5,13 @@ set -x
 git submodule update --init
 curl -sS https://getcomposer.org/installer | php
 ./composer.phar install
+
+rm -rf ./output_prod
+
 ./vendor/bin/sculpin generate --env=prod
-cp -r ./presentations output_prod/presentations
+
+cp -r ./presentations/ output_prod/presentations/
+rm -rf ./output_prod/presentations/*/.git
 
 rm -rf ./gh-pages-deployment
 git clone git@github.com:Ocramius/ocramius.github.com.git ./gh-pages-deployment
