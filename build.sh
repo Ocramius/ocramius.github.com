cebd6c569c2b91a0d0577775cbf0a4f2b0d986cd
Marco Pivetta <ocramius@gmail.com>
2014-10-05 04:12:32 +0200

    Copying presentations as part of the build steps

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+set -x
+
+git submodule update --init
 curl -sS https://getcomposer.org/installer | php
 ./composer.phar install
 ./vendor/bin/sculpin generate --env=prod
+cp -r ./presentations output_prod/presentations
 
 rm -rf ./gh-pages-deployment
 git clone git@github.com:Ocramius/ocramius.github.com.git ./gh-pages-deployment
 cd gh-pages-deployment
-git submodule update --init
-cp -r ./presentations ./../output_prod/presentations
+#git submodule update --init
+#cp -r ./presentations ./../output_prod/presentations
 git checkout gh-pages
 git checkout -b gh-pages
 
 rsync --quiet --archive --filter="P .git*" --exclude=".*.sw*" --exclude=".*.un~" --delete ../output_prod/ ./
-git git add -A :/
+git add -A :/
 git commit -a -m "Deploying sculpin-generated pages to \`gh-pages\` branch"
