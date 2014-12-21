da5164c762f80dfe8b01a595d1531f844bb23121
malukenho <admin@phpse.net>
2014-12-20 23:32:37 -0300

    Remove password an put template key on config.example.php

--- config.example.php
+++ config.example.php
@@ -6,7 +6,23 @@
 return [
     'joindin' => [
         'user' => 'ocramius',
-        'password' => '123'
+        'template' => function($talks) {
+
+            $templatePattern = '<h3>%s</h3><p><small>%s</small></p>'
+                             . '<a href="%s" target="_blank">See on joind.in</a>';
+
+            $template = '<h1>My talks <small><i>(joind.in)</i></small></h1><hr />';
+
+            foreach ($talks['talks'] as $talk) {
+                $template .= sprintf(
+                    $templatePattern,
+                    $talk['talk_title'],
+                    $talk['talk_description'],
+                    $talk['website_uri']
+                );
+            }
+
+            return $template;
+        },
     ]
 ];
-
